c0f7f0dacb40973e6245f28a9a00b3bf95b42b2a
GEMMEE <jr.gamachu@gmail.com>
2022-09-04 13:59:44 +0530

    comparing floats in if condition

--- n-equal.cpp
+++ n-equal.cpp
@@ -8,8 +8,10 @@ int main()
 		cout << "equal" << endl;
 	else cout << "not equal" << endl;
 
-	cout << "AFTER SEEING THE OUTPUT, YOU MIGHT SAY WHAT THE HECK IS HAPPENING?\n" << "a = " << a << endl
+	cout << "YOU MIGHT SAY WHAT THE HECK IS HAPPENING?\n" 
+		 << "a = " << a << endl
 		 << "DON'T BE SURPRISED!\n";
+
 // Most floating point values have no exact binary represenation and 
 // have a limited precision. This means different compilers and CPU
 // architectures store the temporary results at different precisions,
