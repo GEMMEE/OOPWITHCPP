bf7c119788f947c4eedebec7906722c9f350a11c
Jaba <jr.gamachu@gmail.com>
2022-11-13 21:42:25 +0530

    Guess me 3

---
+++ U8-Inheritance_Extending_Classes/dryrun8-3.cpp
@@ -0,0 +1,64 @@
+/**
+ * purpose: What will happen when the following program is run?
+ * author: Jaba
+ * date: Nov 13, 2022
+*/
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int a;
+    public:
+    A() {}
+    A(int d)
+    {
+        a = d;
+    }
+    ~ A()
+    {
+        cout << endl << "Object A being destroyed";
+    }
+    void show()
+    {
+        cout << a << endl;
+    }
+};
+class B : public A
+{
+    int b;
+    public:
+    B() : A()
+    {
+    }
+    B(int d, int d1) : A(d)
+    {
+        b = d1;
+    }
+    ~ B()
+    {
+        cout << endl << "Object B is being destroyed";
+    }
+    void show()
+    {
+        A :: show();
+        cout << b << endl;
+    }
+};
+
+int main()
+{
+    B *p;
+    p = new B(20, 30);
+    (*p).show();
+    delete p;
+    return 0;
+}
+
+/*------------------ My prediction (+) --------------
+20
+30
+
+Object B is being destroyed
+Object A being destroyed  ---------------------------*/
